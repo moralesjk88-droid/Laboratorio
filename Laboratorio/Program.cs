a6a9421ef87012800de9f25cc6c827f2b8e16c09
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-23 19:05:29 -0600

    constructor hecho

--- Laboratorio/Program.cs
+++ Laboratorio/Program.cs
@@ -1,1 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+
+public class Prestamo
+{
+    private string codigo;
+    private string estudiante;
+    private string carnet;
+    private string carrera;
+    private string equipo;
+    private int cantidad;
+    private string estado;
+
+    public Prestamo (string codigo, string estudiante, string carnet, string carrera, string equipo, int cantidad, string estado)
+    {
+        this.codigo = codigo;
+        this.estudiante = estudiante;
+        this.carnet = carnet;
+        this.carrera = carrera;
+        this.equipo = equipo;
+        this.cantidad = cantidad;
+        this.estado = estado;
+    }
+
+    public string ObtenerDatos()
+    {
+        return "Cod: " + codigo + " | Estudiante: " + estudiante + " | Equipo: " + equipo + " | Estado: " + estado;
+    }
+}
