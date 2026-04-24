b3929f9a7d8641d20bbdee5060c7d826eff7df09
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-23 20:40:37 -0600

    terminado

--- Laboratorio/Program.cs
+++ Laboratorio/Program.cs
@@ -57,6 +57,118 @@ class Program
 {
     static void Main()
     {
+        string ruta = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+            "prestamo.txt");
+       
+
+        Dictionary<string,Prestamo>prestamo= new Dictionary<string,Prestamo>();
+        int opcion = 0;
+        
+
+        do
+        {
+            Console.WriteLine("\n **Sistema de precios**");
+            Console.WriteLine("1. Registrar prestamo");
+            Console.WriteLine("2. Buscar Codigo");
+            Console.WriteLine("3. Mostrar todos los registros");
+            Console.WriteLine("4. Eliminar");
+            Console.WriteLine("5. Salir");
+            Console.WriteLine("Seleccione una opcion");
+            if (!int.TryParse(Console.ReadLine(), out opcion))
+            {
+                Console.WriteLine("Ingrese un valor valido");
+                Console.ReadKey();
+                continue;
+            }
+            
+            switch(opcion)
+            {
+                case 1:
+                    Console.WriteLine("\n--- REGISTRO DE DATOS ---");
+                    Console.Write("Código: "); 
+                    string codigo = Console.ReadLine();
+                    Console.Write("Nombre: ");
+                    string estudiante = Console.ReadLine();
+                    Console.Write("Carnet: ");
+                    string carnet = Console.ReadLine();
+                    Console.Write("Carrera: ");
+                    string carrera = Console.ReadLine();
+                    Console.Write("Equipo: "); 
+                    string equipo = Console.ReadLine();
+                    Console.Write("Cantidad: ");
+                    int cantidad;
+                    while (!int.TryParse(Console.ReadLine(), out cantidad))
+                    {
+                        Console.WriteLine("Ingrese un número para la cntidad");
+                    }     
+                    Console.Write("Estado: "); 
+                    string estado = Console.ReadLine();
+
+                    Prestamo p=new Prestamo(codigo,estudiante, carnet, carrera,equipo,cantidad,estado);
+
+                    prestamo.Add(p.Codigo, p);
+                    p.GuardarEnArchivo(ruta);
+                    Console.WriteLine("\n Cuardado correctamente");
+                   Console.ReadKey();
+                    break;
+                case 2:
+                    Console.WriteLine("\n Ingrese cosigo a buscar ");
+                        string buscar= Console.ReadLine();
+                    if (prestamo.ContainsKey(buscar))
+                    {
+                        Console.WriteLine("\n Resultado");
+                        Console.WriteLine(prestamo[buscar].ResumenConsola());
+                    }
+                    else
+                    {
+                        Console.WriteLine("codigo no encontrado");
+                    }
+                    Console.ReadKey();
+                    break;
+                    case 3:
+                    Console.WriteLine("\n Listado de memoria");
+                    if (prestamo.Count==0)
+                    {
+                        Console.WriteLine("No hay datos registrados");
+                    }
+                    else
+                    {
+                        foreach (Prestamo item in prestamo.Values)
+                        {
+                            Console.WriteLine(item.ResumenConsola());
+                        }
+                    }
+                    Console.ReadKey ();
+                    break;
+                    case 4:
+                    Console.WriteLine("Codigo a eliminar");
+                    string eliminar= Console.ReadLine();
+                    if (prestamo.Remove(eliminar))
+                    {
+                        Console.WriteLine("Eliminado");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Codigo no encontrado");
+                    }
+                    Console.ReadKey();
+                    break;
+                    case 5:
+                    Console.WriteLine("Saliendo del programa");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Opción no valida");
+                    Console.ReadKey();
+                    break;
+            }
+
+
+
+
+        }
+        while (opcion!=5);
 
     }
 
